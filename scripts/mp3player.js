dadaff893a383ae4f951977a01c0278c9c51c50d
vo41 <161795582+vo41@users.noreply.github.com>
2024-03-01 18:25:40 -0300

    Delete scripts/mp3player.js

--- scripts/mp3player.js
+++
@@ -1,9 +0,0 @@
-// script.js or mp3player.js
-function loadPlayerContent() {
-    fetch('mp3player.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('playerContent').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading player content:', error));
-}
