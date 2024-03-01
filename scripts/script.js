46a0af42857e889a2429b03ab4eac6139591dc2b
vo41 <161795582+vo41@users.noreply.github.com>
2024-03-01 11:02:16 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -27,30 +27,4 @@ document.addEventListener('DOMContentLoaded', function () {
             showSubPage(pageId);
         });
     });
-
-document.addEventListener('DOMContentLoaded', function () {
-    const openButton = document.getElementById('openPlayerModal');
-    const modal = document.getElementById('playerModal');
-    const closeButton = document.getElementById('closePlayerModal');
-    const playerContent = document.getElementById('playerContent');
-
-    openButton.addEventListener('click', function () {
-        modal.style.display = 'block';
-        // Load the content of player.html into the modal
-        loadPlayerContent();
-    });
-
-    closeButton.addEventListener('click', function () {
-        modal.style.display = 'none';
-    });
-
-    function loadPlayerContent() {
-        // Use fetch or another method to load the content of player.html
-        fetch('player.html')
-            .then(response => response.text())
-            .then(data => {
-                playerContent.innerHTML = data;
-            })
-            .catch(error => console.error('Error loading player content:', error));
-    }
 });
