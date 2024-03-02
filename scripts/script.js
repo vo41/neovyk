385a9b88e2d91a1d8e74f03b9158b6772b22f5e7
vo41 <161795582+vo41@users.noreply.github.com>
2024-03-02 03:07:15 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,7 @@ function openWindow(contentId) {
     const contentWindow = document.getElementById(contentId);
 
     // Bring the opened window to the front
-    const allWindows = document.querySelectorAll('.sub-page');
-    let highestZIndex = 0;
-
-    allWindows.forEach((window) => {
-        const zIndex = parseInt(window.style.zIndex || 0, 10);
-        highestZIndex = Math.max(highestZIndex, zIndex);
-    });
-
-    contentWindow.style.zIndex = highestZIndex + 1;
+    contentWindow.style.zIndex = Date.now();
 
     // If the content is an audio player, initialize it
     if (contentId === 'player') {
@@ -40,13 +32,13 @@ const subPages = document.querySelectorAll('.sub-page');
 // Add click event listeners to each menu item
 menuItems.forEach((menuItem, index) => {
     menuItem.addEventListener('click', (event) => {
-        // Add 'active' class to the selected sub-page
-        subPages[index].classList.add('active');
+        // Close all active windows
+        subPages.forEach((subPage) => {
+            subPage.classList.remove('active');
+        });
 
-        // Get the contentId from the data-window attribute
+        // Bring the clicked window to the front
         const contentId = menuItem.getAttribute('data-window');
-
-        // Open the window for the specified content
         openWindow(contentId);
     });
 });
